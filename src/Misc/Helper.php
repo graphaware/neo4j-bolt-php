9164b7c928e135a6e5c570243847c87714c57647
Christophe Willemsen <willemsen.christophe@gmail.com>
2015-08-31 00:57:24 +0200

    License files

--- src/Misc/Helper.php
+++ src/Misc/Helper.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the GraphAware Bolt package.
+ *
+ * (c) GraphAware Ltd <christophe@graphaware.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace GraphAware\Bolt\Misc;
 
 class Helper
--- src/PackStream/Packer.php
+++ src/PackStream/Packer.php
@@ -12,11 +12,14 @@
 namespace GraphAware\Bolt\PackStream;
 
 use GraphAware\Bolt\Exception\SerializationException;
-use GraphAware\Bolt\Misc\Helper;
 use GraphAware\Bolt\Protocol\Constants;
 
 class Packer
 {
+    /**
+     * @param $v
+     * @return string
+     */
     public function pack($v)
     {
         $stream = '';
@@ -39,145 +42,11 @@ class Packer
         return $stream;
     }
 
-    public function isList(array $array)
-    {
-        foreach ($array as $k => $v) {
-            if (!is_int($k)) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    public function packList(array $array)
-    {
-        $size = count($array);
-        $b = $this->getListSizeMarker($size);
-        foreach ($array as $k => $v) {
-            $b .= $this->pack($v);
-        }
-
-        return $b;
-    }
-
-    public function packMap(array $array)
-    {
-        $size = count($array);
-        $b = '';
-        $b .= $this->getMapSizeMarker($size);
-        foreach ($array as $k => $v) {
-            $b .= $this->pack((string) $k);
-            $b .= $this->pack($v);
-        }
-
-        return $b;
-    }
-
-    public function getListSizeMarker($size)
-    {
-        $b = '';
-        if ($size < Constants::SIZE_TINY) {
-            $b .= chr(Constants::LIST_TINY + $size);
-            return $b;
-        }
-        if ($size < Constants::SIZE_8) {
-            $b .= chr(Constants::LIST_8);
-            $b .= $this->packUnsignedShortShort($size);
-            return $b;
-        }
-        if ($b < Constants::SIZE_16) {
-            $b .= chr(Constants::LIST_16);
-            $b .= $this->packUnsignedShort($size);
-            return $b;
-        }
-        if ($b < Constants::SIZE_32) {
-            $b .= chr(Constants::LIST_32);
-            $b .= $this->packUnsignedLong($size);
-            return $b;
-        }
-
-        throw new SerializationException(sprintf('Unable to create marker for List size %d', $size));
-    }
-
-    public function getMapSizeMarker($size)
-    {
-        $b = '';
-        if ($size < Constants::SIZE_TINY) {
-            $b .= chr(Constants::MAP_TINY + $size);
-            return $b;
-        }
-        if ($size < Constants::SIZE_8) {
-            $b .= chr(Constants::MAP_8);
-            $b .= $this->packUnsignedShortShort($size);
-            return $b;
-        }
-        if ($size < Constants::SIZE_16) {
-            $b .= chr(Constants::MAP_16);
-            $b .= $this->packUnsignedShort($size);
-            return $b;
-        }
-        if ($size < Constants::SIZE_32) {
-            $b .= chr(Constants::MAP_32);
-            $b .= $this->packUnsignedLong($size);
-            return $b;
-        }
-
-
-        throw new SerializationException(sprintf('Unable to pack Array with size %d. Out of bound !', $size));
-    }
-
-    public function packText($value)
-    {
-        $value = utf8_decode($value);
-        $length = strlen($value);
-        $b = '';
-        if ($length < 16) {
-            $b .= chr(Constants::TEXT_TINY + $length);
-            $b .= $value;
-            return $b;
-        }
-
-        if ($length < 256) {
-            $b .= chr(Constants::TEXT_8);
-            $b .= $this->packUnsignedShortShort($length);
-            $b .= $value;
-            return $b;
-        }
-
-        if ($length < 65536) {
-            $b .= chr(Constants::TEXT_16);
-            $b .= $this->packUnsignedShort($length);
-            $b .= $value;
-            return $b;
-        }
-
-        if ($length < PHP_INT_MAX && $length < 4294967295) {
-            $b .= chr(Constants::INT_32);
-            $b .= $this->packUnsignedLong($length);
-            $b .= $value;
-            return $b;
-        }
-
-        throw new \OutOfBoundsException(sprintf('The value %s can not be packed, length is out of bound', $value));
-    }
-
-    public function getRunSignature()
-    {
-        return chr(Constants::SIGNATURE_RUN);
-    }
-
-    public function getEndSignature()
-    {
-        return str_repeat(chr(Constants::MISC_ZERO), 2);
-    }
-
-    public function getSizeMarker($stream)
-    {
-        $size = mb_strlen($stream, 'ASCII');
-
-        return pack('n', $size);
-    }
-
+    /**
+     * @param $length
+     * @param $signature
+     * @return string
+     */
     public function packStructureHeader($length, $signature)
     {
         $stream = '';
@@ -227,11 +96,192 @@ class Packer
         return $bytes;
     }
 
+    /**
+     * @param array $array
+     * @return string
+     */
+    public function packList(array $array)
+    {
+        $size = count($array);
+        $b = $this->getListSizeMarker($size);
+        foreach ($array as $k => $v) {
+            $b .= $this->pack($v);
+        }
+
+        return $b;
+    }
+
+    /**
+     * @param array $array
+     * @return bool
+     */
+    public function isList(array $array)
+    {
+        foreach ($array as $k => $v) {
+            if (!is_int($k)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * @param $size
+     * @return string
+     */
+    public function getListSizeMarker($size)
+    {
+        $b = '';
+        if ($size < Constants::SIZE_TINY) {
+            $b .= chr(Constants::LIST_TINY + $size);
+            return $b;
+        }
+        if ($size < Constants::SIZE_8) {
+            $b .= chr(Constants::LIST_8);
+            $b .= $this->packUnsignedShortShort($size);
+            return $b;
+        }
+        if ($b < Constants::SIZE_16) {
+            $b .= chr(Constants::LIST_16);
+            $b .= $this->packUnsignedShort($size);
+            return $b;
+        }
+        if ($b < Constants::SIZE_32) {
+            $b .= chr(Constants::LIST_32);
+            $b .= $this->packUnsignedLong($size);
+            return $b;
+        }
+
+        throw new SerializationException(sprintf('Unable to create marker for List size %d', $size));
+    }
+
+    /**
+     * @param array $array
+     * @return string
+     */
+    public function packMap(array $array)
+    {
+        $size = count($array);
+        $b = '';
+        $b .= $this->getMapSizeMarker($size);
+        foreach ($array as $k => $v) {
+            $b .= $this->pack((string) $k);
+            $b .= $this->pack($v);
+        }
+
+        return $b;
+    }
+
+    /**
+     * @param $size
+     * @return string
+     */
+    public function getMapSizeMarker($size)
+    {
+        $b = '';
+        if ($size < Constants::SIZE_TINY) {
+            $b .= chr(Constants::MAP_TINY + $size);
+            return $b;
+        }
+        if ($size < Constants::SIZE_8) {
+            $b .= chr(Constants::MAP_8);
+            $b .= $this->packUnsignedShortShort($size);
+            return $b;
+        }
+        if ($size < Constants::SIZE_16) {
+            $b .= chr(Constants::MAP_16);
+            $b .= $this->packUnsignedShort($size);
+            return $b;
+        }
+        if ($size < Constants::SIZE_32) {
+            $b .= chr(Constants::MAP_32);
+            $b .= $this->packUnsignedLong($size);
+            return $b;
+        }
+
+
+        throw new SerializationException(sprintf('Unable to pack Array with size %d. Out of bound !', $size));
+    }
+
+    /**
+     * @param $value
+     * @return string
+     */
+    public function packText($value)
+    {
+        $value = utf8_decode($value);
+        $length = strlen($value);
+        $b = '';
+        if ($length < 16) {
+            $b .= chr(Constants::TEXT_TINY + $length);
+            $b .= $value;
+            return $b;
+        }
+
+        if ($length < 256) {
+            $b .= chr(Constants::TEXT_8);
+            $b .= $this->packUnsignedShortShort($length);
+            $b .= $value;
+            return $b;
+        }
+
+        if ($length < 65536) {
+            $b .= chr(Constants::TEXT_16);
+            $b .= $this->packUnsignedShort($length);
+            $b .= $value;
+            return $b;
+        }
+
+        if ($length < PHP_INT_MAX && $length < 4294967295) {
+            $b .= chr(Constants::INT_32);
+            $b .= $this->packUnsignedLong($length);
+            $b .= $value;
+            return $b;
+        }
+
+        throw new \OutOfBoundsException(sprintf('The value %s can not be packed, length is out of bound', $value));
+    }
+
+    /**
+     * @return string
+     */
+    public function getRunSignature()
+    {
+        return chr(Constants::SIGNATURE_RUN);
+    }
+
+    /**
+     * @return string
+     */
+    public function getEndSignature()
+    {
+        return str_repeat(chr(Constants::MISC_ZERO), 2);
+    }
+
+    /**
+     * @param $stream
+     * @return string
+     */
+    public function getSizeMarker($stream)
+    {
+        $size = mb_strlen($stream, 'ASCII');
+
+        return pack('n', $size);
+    }
+
+    /**
+     * @param $v
+     * @return string
+     */
     public function packBigEndian($v)
     {
         return pack('N', $v);
     }
 
+    /**
+     * @param $value
+     * @return string
+     */
     public function packInteger($value)
     {
         $value = (int) $value;
@@ -249,26 +299,46 @@ class Packer
         }
     }
 
+    /**
+     * @param $integer
+     * @return string
+     */
     public function packUnsignedShortShort($integer)
     {
         return pack('C', $integer);
     }
 
+    /**
+     * @param $integer
+     * @return string
+     */
     public function packSignedShort($integer)
     {
         return pack('s', $integer);
     }
 
+    /**
+     * @param $integer
+     * @return string
+     */
     public function packUnsignedShort($integer)
     {
         return pack('n', $integer);
     }
 
+    /**
+     * @param $integer
+     * @return string
+     */
     public function packUnsignedLong($integer)
     {
         return pack('N', $integer);
     }
 
+    /**
+     * @param $value
+     * @return bool
+     */
     public function isShortShort($value)
     {
         if (in_array($value, range(-16, 127))) {
@@ -278,6 +348,10 @@ class Packer
         return false;
     }
 
+    /**
+     * @param $integer
+     * @return bool
+     */
     public function isShort($integer)
     {
         $min = 128;
