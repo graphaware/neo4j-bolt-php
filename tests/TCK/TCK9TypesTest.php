346e170a880d80b5364d632dde5c1136d7b34646
Christophe Willemsen <willemsen.christophe@gmail.com>
2015-12-21 15:40:57 +0100

    more typing tests

--- tests/TCK/TCK9TypesTest.php
+++ tests/TCK/TCK9TypesTest.php
@@ -82,10 +82,24 @@ class TCK9TypesTest extends TCKTestCase
         $result = $session->run("CREATE (n) RETURN n.key as nilKey");
         $this->assertEquals(null, $result->getRecord()->value('nilKey'));
 
+        // collection of null
+        $result = $session->run("UNWIND range(0, 2) as r CREATE (n) RETURN collect(n.x) as x");
+        $this->assertInternalType('array', $result->getRecord()->value('x'));
+        foreach ($result->getRecord()->value('x') as $v) {
+            $this->assertEquals(null, $v);
+        }
+
         // boolean
         $result = $session->run("CREATE (n) RETURN id(n) = id(n) as bool, id(n) = 'a' as bool2");
         $this->assertEquals(true, $result->getRecord()->value('bool'));
         $this->assertEquals(false, $result->getRecord()->value('bool2'));
+
+        // collection of booleans
+        $result = $session->run("UNWIND range(0, 2) as r CREATE (n) SET n.x = (id(n) = id(n)) RETURN collect(n.x) as x");
+        $this->assertInternalType('array', $result->getRecord()->value('x'));
+        foreach ($result->getRecord()->value('x') as $v) {
+            $this->assertEquals(true, $v);
+        }
 
         // string
         $result = $session->run("CREATE (n {k: {value}}) RETURN n.k as v", ['value' => 'text']);
@@ -95,16 +109,38 @@ class TCK9TypesTest extends TCKTestCase
         $result = $session->run("CREATE (n {k: {value}}) RETURN n.k as v", ['value' => 1.38]);
         $this->assertEquals(1.38, $result->getRecord()->value('v'));
 
+        // collection of floats
+        $result = $session->run("UNWIND range(0,2) as r CREATE (n:X) SET n.k = (id(n) / 100.0f) RETURN collect(n.k) as x");
+        $this->assertCount(3, $result->getRecord()->value('x'));
+        foreach ($result->getRecord()->value('x') as $v) {
+            $this->assertInternalType('float', $v);
+        }
+
         // int
         $result = $session->run("CREATE (n) RETURN id(n) as id");
         $this->assertInternalType('int', $result->getRecord()->value('id'));
         $this->assertTrue($result->getRecord()->value('id') >= 0);
+
+        // collection of integers
+        $result = $session->run("UNWIND range(0, 2) as r CREATE (n:X) SET n.k = id(n) RETURN collect(n.k) as x");
+        $this->assertCount(3, $result->getRecord()->value('x'));
+        foreach ($result->getRecord()->value('x') as $v) {
+            $this->assertInternalType('int', $v);
+        }
 
         // list<any>
         $result = $session->run("CREATE (n:Person:Male) RETURN labels(n) as l");
         $this->assertInternalType('array', $result->getRecord()->value('l'));
         $this->assertTrue(array_key_exists(0, $result->getRecord()->value('l')));
         $this->assertTrue(array_key_exists(1, $result->getRecord()->value('l')));
+
+        // collection of list<any>
+        $result = $session->run("UNWIND range(0, 2) as r CREATE (n:X) RETURN collect(labels(n)) as x");
+        $this->assertCount(3, $result->getRecord()->value('x'));
+        foreach ($result->getRecord()->value('x') as $v) {
+            $this->assertInternalType('array', $v);
+            $this->assertEquals('X', $v[0]);
+        }
 
         // map<string:any>
         $result = $session->run("CREATE (n:Node) RETURN {id: id(n), labels: labels(n)} as map");
@@ -114,6 +150,15 @@ class TCK9TypesTest extends TCKTestCase
         $this->assertInternalType('int', $result->getRecord()->value('map')['id']);
         $this->assertInternalType('array', $result->getRecord()->value('map')['labels']);
 
+        // collection of map<string:any>
+        $result = $session->run("UNWIND range(0, 2) as r CREATE (n:X) RETURN collect({labels: labels(n)}) as x");
+        $this->assertCount(3, $result->getRecord()->value('x'));
+        foreach ($result->getRecord()->value('x') as $v) {
+            $this->assertInternalType('array', $v);
+            $this->assertArrayHasKey('labels', $v);
+            $this->assertEquals('X', $v['labels'][0]);
+        }
+
         // node
         $result = $session->run("CREATE (n:Node) RETURN n");
         $this->assertInstanceOf(NodeInterface::class, $result->getRecord()->value('n'));
@@ -122,9 +167,21 @@ class TCK9TypesTest extends TCKTestCase
         // collection of nodes
         $result = $session->run("UNWIND range(0,2) as r CREATE (n:Node {value: r}) RETURN collect(n) as n");
         $this->assertCount(3, $result->getRecord()->value('n'));
-        foreach ($result->getRecord()->value('n') as $n) {
+        foreach ($result->getRecord()->value('n') as $k => $n) {
             $this->assertInstanceOf(NodeInterface::class, $n);
+            $this->assertEquals($k, $n->value('value'));
         }
+
+        // map<string:node>
+        $result = $session->run("CREATE (n:X) RETURN {created: n} as x");
+        $this->assertInstanceOf(NodeInterface::class, $result->getRecord()->value('x')['created']);
+
+        // collection of map<string:<collection node>>
+        $result = $session->run("UNWIND range(0, 2) as r CREATE (n:X) WITH collect(n) as n RETURN collect({nodes: n}) as x");
+        $this->assertCount(1, $result->getRecord()->value('x'));
+        $this->assertArrayHasKey('nodes', $result->getRecord()->value('x')[0]);
+        $this->assertCount(3, $result->getRecord()->value('x')[0]['nodes']);
+        $this->assertInstanceOf(NodeInterface::class, $result->getRecord()->value('x')[0]['nodes'][0]);
     }
 
     private function runValue($value)
