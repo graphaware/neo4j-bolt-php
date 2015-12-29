75e82546381d4f3934b9814b8c067b54a70287d3
Christophe Willemsen <willemsen.christophe@gmail.com>
2015-12-29 13:12:28 +0100

    use M02 for CI

--- build/install-neo.sh
+++ build/install-neo.sh
@@ -3,7 +3,7 @@
 export JAVA_HOME=/usr/lib/jvm/java-8-oracle
 export JRE_HOME=/usr/lib/jvm/java-8-oracle
 
-wget http://alpha.neohq.net/dist/neo4j-enterprise-3.0.0-M01-NIGHTLY-unix.tar.gz > null
+wget http://alpha.neohq.net/dist/neo4j-enterprise-3.0.0-M02-NIGHTLY-unix.tar.gz > null
 mkdir neo
 tar xzf neo4j-enterprise-3.0.0-M01-NIGHTLY-unix.tar.gz -C neo --strip-components=1 > null
 sed -i.bak 's/^\(dbms\.security\.auth_enabled=\).*/\1false/' ./neo/conf/neo4j-server.properties
