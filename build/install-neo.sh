0be505661f3312d8d9bcc2b07703dd360bf33727
Christophe Willemsen <willemsen.christophe@gmail.com>
2015-12-30 01:35:01 +0100

    fixed bad commenting

--- build/install-neo.sh
+++ build/install-neo.sh
@@ -7,7 +7,7 @@ wget http://alpha.neohq.net/dist/neo4j-enterprise-3.0.0-M02-NIGHTLY-unix.tar.gz 
 mkdir neo
 tar xzf neo4j-enterprise-3.0.0-M02-NIGHTLY-unix.tar.gz -C neo --strip-components=1 > null
 sed -i.bak 's/^\(dbms\.security\.auth_enabled=\).*/\1false/' ./neo/conf/neo4j-server.properties
-//sed -i.bak 's/^\(dbms\.bolt\.tls\.enabled=\).*/\1true/' ./neo/conf/neo4j-server.properties
+#sed -i.bak 's/^\(dbms\.bolt\.tls\.enabled=\).*/\1true/' ./neo/conf/neo4j-server.properties
 neo/bin/neo4j start > null &
-//sleep 10
-//mv neo/conf/ssl/snakeoil.cert neo/conf/ssl/snakeoil.pem
+#sleep 10
+#mv neo/conf/ssl/snakeoil.cert neo/conf/ssl/snakeoil.pem
